7fd1a6d23734e81f53eec46e226401b494d8cc36
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-11-24 11:17:09 -0800

    feat: add application command models

--- discatpy/models/__init__.py
+++ discatpy/models/__init__.py
@@ -8,6 +8,7 @@ All of the Discord API models for `discatpy`.
 from .abc import *
 from .asset import *
 from .color import *
+from .command import *
 from .embed import *
 from .emoji import *
 from .iterators import *
@@ -19,6 +20,7 @@ __all__ = ()
 __all__ += abc.__all__
 __all__ += asset.__all__
 __all__ += color.__all__
+__all__ += command.__all__
 __all__ += embed.__all__
 __all__ += emoji.__all__
 __all__ += iterators.__all__
